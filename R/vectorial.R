778c2d90d149abe8f9ccb248d642dd04acf023f5
castrocanabal2 <castrocanabal2@gmail.com>
2026-02-23 20:52:41 +0100

    creación de paquete

---
+++ R/vectorial.R
@@ -0,0 +1,34 @@
+#'
+#'
+
+#Crear una versión de la herramienta buffer
+
+#la herramienta depende de los siguientes paquetes
+library(sf)
+library(dplyr)
+
+# toma una capa vectorial de entrada
+# requiere de especificar una distancia de vecindad
+# Opciones:
+#  - Fusionar o no geometrías
+#  - Fusionar en función de un atributo
+#  - Mantener atributos en la capa resultante
+
+parcelas <- read_sf("C:/Users/AlumnoMaster/Desktop/Programacion/parcelas") |>
+  mutate(Especie=1)
+parcelas_buffer <- st_buffer(parcelas, dist=10000)|>
+  #group_by(Especie) |>
+  summarise(geometry = st_union(geometry))
+
+sig_buffer <- function(capa_entrada, distancia, dissolve=TRUE){
+  if(dissolve==TRUE){
+    buffer <- st_buffer(capa_entrada, dist=distancia)|>
+      summarise(geometry = st_union(geometry))
+  }else{
+    buffer <- st_buffer(capa_entrada, dist =distancia)
+  }
+  return(buffer)
+}
+
+parcelas_buffer_function <-sig_buffer(parcelas, 5000, dissolve =FALSE)
+plot(parcelas_buffer_function)
